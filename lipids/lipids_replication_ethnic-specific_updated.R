c2f8e78ef61c6c27cd85f9154cb476b24863e1fb
AustinTHilliard <42285131+AustinTHilliard@users.noreply.github.com>
2018-09-18 16:46:45 -0700

    Add files via upload

--- lipids/lipids_replication_ethnic-specific_updated.R
+++ lipids/lipids_replication_ethnic-specific_updated.R
@@ -342,10 +342,12 @@ rownames(xdf_matched_traits) = NULL
 dim(xdf_matched_traits)
 # [1] 162  19
 
+# order based on trait, chromosome, and race
 xord = order(xdf_matched_traits$Trait, xdf_matched_traits$CHR, xdf_matched_traits$MVP.race)
 xdf_matched_traits = xdf_matched_traits[xord, ]
 rm(xord)
 
+# look at distributions of race and traits
 table(subset(xdf_matched_traits, is.na(MVP.alleleA))$MVP.race)
 # AFR EUR HIS 
 #  19  27  19 
@@ -359,15 +361,12 @@ sort(table(apply(xdf_matched_traits[,c("Trait","MVP.race")],
 # LDL:AFR LDL:HIS LDL:EUR  TC:AFR  TC:HIS HDL:AFR HDL:HIS  TC:EUR  TG:AFR  TG:HIS HDL:EUR  TG:EUR 
 #       1       1       3       4       4       6       6       6       8       8       9       9 
 
-# 
-miss_snps = lapply(split(subset(xdf_matched_traits, is.na(MVP.alleleA))$MarkerName,  
-                         subset(xdf_matched_traits, is.na(MVP.alleleA))$Trait), unique)
-
-#
+# plot histograms of MVP p-values for each trait
 par(mfrow=c(2,2))
 xcuts = c(0, 1e-8, 1e-6, 1e-4, 1e-2, 1e-1, 1)
 for (tr in c("HDL","TC","LDL","TG")) {
-    barplot(table(cut(as.numeric(xdf_matched_traits$MVP.frequentist_add_pvalue)[xdf_matched_traits$Trait == tr], xcuts)),
+    toplot = xdf_matched_traits$MVP.frequentist_add_pvalue[xdf_matched_traits$Trait == tr]
+    barplot(table(cut(as.numeric(toplot), xcuts)),
             main=tr, col='lightgrey')
     abline(h=5, col='darkgrey')
 }
@@ -377,81 +376,135 @@ xdf_m_spl = split(xdf_matched_traits[,c(1,2,5:7)], xdf_matched_traits$MarkerName
 # # ------------------------------------------------------------------------------                                       
 # # -- get proxies for PAGE snps missing from MVP
 # # ------------------------------------------------------------------------------
-# # assumes miss_snps_proxies, xtraits, and xlist exist in workspace
-# .getProxyForSnp = function (missing_snp) {
-#     snp_rows = miss_snps_proxies$Index.MarkerName == missing_snp
-#     prox = subset(miss_snps_proxies, snp_rows)$Proxy.MarkerName
-#     prox = prox[prox != missing_snp]
-#     trait = unique(subset(miss_snps_proxies, snp_rows)$Trait.s.)
-#     if (grepl("_", trait)) {
-#         trait = unlist(strsplit(trait, "_"))
-#     }
-#     mvp_trait = xlist[xtraits %in% trait]
-#     mvp_proxies = list()
-#     for (race_eth in 1:length(mvp_trait)) {
-#         xmvp = mvp_trait[[race_eth]]
-#         xmvp_name = names(mvp_trait)[race_eth]
-#         xmvp = xmvp[match(prox, xmvp$rsid), ]
-#         xmvp$rsid = prox
-#         if (!all(is.na(xmvp))) {
-#             NArows = apply(xmvp, 1, function(f) all(is.na(f)))
-#             xmvp = xmvp[!NArows, ]
-#         }
-#         mvp_proxies[[xmvp_name]] = xmvp
-#     }
-#     mvp_proxies = as.data.frame(do.call(rbind, mvp_proxies))
-#     spl_rownames = strsplit(rownames(mvp_proxies), "\\.")
-#     
-#     mvp_proxies$Trait = sapply(spl_rownames, function(f) f[3])
-#     mvp_proxies$Index.MarkerName = rep(missing_snp, nrow(mvp_proxies))
-#     mvp_proxies$MVP.race = sapply(spl_rownames, function(f) f[2])
-#     
-#     mvp_proxies = mvp_proxies[, c(17:19, 1:16)]
-#     names(mvp_proxies)[-c(1:3)] = paste0("Proxy.MVP.", 
-#                                          names(mvp_proxies)[-c(1:3)])
-#     rownames(mvp_proxies) = NULL
-#     return(list(Proxy.MarkerName=prox, Trait=trait, MVP=mvp_proxies))
-# }
-# # get names of missing snps and their proxies
-# miss_snps = unique(subset(xdf_matched_traits, is.na(MVP.alleleA))$MarkerName)
-# miss_snps_proxies = subset(snps_page_proxies, Index.MarkerName %in% miss_snps)
-# 
-# #
-# mvp_proxies = lapply(as.list(miss_snps), .getProxyForSnp)
-# names(mvp_proxies) = miss_snps
-# 
-# for (m in 1:length(mvp_proxies)) {
-#     if (nrow(mvp_proxies[[m]]$MVP) == 0) {
-#         mvp_proxies[[m]]$MVP = mvp_proxies[[m]]$MVP[1, ]
-#         mvp_proxies[[m]]$MVP$Trait = mvp_proxies[[m]]$Trait
-#         mvp_proxies[[m]]$MVP$Index.MarkerName = names(mvp_proxies)[m]
-#         
-#         x = subset(xdf_matched_traits, MarkerName == names(mvp_proxies)[m])
-#         xNA = is.na(x$MVP.alleleA)
-#         if (sum(xNA) > 1) {
-#             mvp_proxies[[m]]$MVP = mvp_proxies[[m]]$MVP[1:sum(xNA), ]
-#             mvp_proxies[[m]]$MVP[, 1:3] = x[, c(1,5,6)]
-#         } else {
-#             mvp_proxies[[m]]$MVP$MVP.race = x$MVP.race[xNA]
-#         }
-#     }
-# }
-# rm(m, x, xNA)
-# 
-# #
-# mvp_proxies_df = as.data.frame(do.call(rbind, lapply(mvp_proxies, 
-#                                                      function(f) f$MVP)))
-# rownames(mvp_proxies_df) = NULL
-# 
-# #
-# flt = subset(xdf_matched_traits, MarkerName %in% miss_snps & is.na(MVP.alleleA))
-# flt = apply(flt[, c("Trait", "MarkerName", "MVP.race")], 1, 
-#             paste0, collapse="_")
-# 
-# tocheck = apply(mvp_proxies_df[, c("Trait", "Index.MarkerName", "MVP.race")], 1,
-#                 paste0, collapse="_")
-# 
-# mvp_proxies_df = mvp_proxies_df[tocheck %in% flt, ]
+
+# assumes miss_snps_proxies, xtraits, and xlist exist in workspace
+.getProxyForSnp = function (missing_snp=NULL, miss_snps_proxies=NULL, 
+                            xtraits=NULL, xlist=NULL) {
+    # get names of proxy snps for the missing snp
+    snp_rows = miss_snps_proxies$Index.MarkerName == missing_snp
+    prox = subset(miss_snps_proxies, snp_rows)$Proxy.MarkerName
+    
+    # get rid of entry that's same as missing snp
+    prox = prox[prox != missing_snp]
+    
+    # get the trait for missing snp
+    trait = unique(subset(miss_snps_proxies, snp_rows)$Trait.s.)
+    
+    # check if missing snp was significant for >1 trait in PAGE
+    if (grepl("_", trait)) {
+        trait = unlist(strsplit(trait, "_"))
+    }
+    
+    # get the MVP results for the trait and loop through races
+    mvp_trait = xlist[xtraits %in% trait]
+    
+    # empty list to hold results from loop
+    mvp_proxies = list()
+    for (race_eth in 1:length(mvp_trait)) {
+        
+        # get results and name for this race
+        xmvp = mvp_trait[[race_eth]]
+        xmvp_name = names(mvp_trait)[race_eth]
+        
+        # subset to rows for proxy snps
+        xmvp = xmvp[match(prox, xmvp$rsid), ]
+        
+        # add back names for any missing proxy snps
+        xmvp$rsid = prox
+        # if (!all(is.na(xmvp))) {
+        #     NArows = apply(xmvp, 1, function(f) all(is.na(f)))
+        #     xmvp = xmvp[!NArows, ]
+        # }
+        
+        # add subsetted MVP results to list
+        mvp_proxies[[xmvp_name]] = xmvp
+    }
+    # collapse list to one data frame and split rownames
+    mvp_proxies = as.data.frame(do.call(rbind, mvp_proxies))
+    spl_rownames = strsplit(rownames(mvp_proxies), "\\.")
+    
+    # add columns for trait, index snp, and race
+    mvp_proxies$Trait = sapply(spl_rownames, function(f) f[3])
+    mvp_proxies$Index.MarkerName = rep(missing_snp, nrow(mvp_proxies))
+    mvp_proxies$MVP.race = sapply(spl_rownames, function(f) f[2])
+    
+    # reorder columns and rename those with results for proxies
+    mvp_proxies = mvp_proxies[, c(17:19, 1:16)]
+    names(mvp_proxies)[-c(1:3)] = paste0("Proxy.MVP.",
+                                         names(mvp_proxies)[-c(1:3)])
+    # clear the rownames
+    rownames(mvp_proxies) = NULL
+    
+    # return list with proxy names, trait, and MVP results for proxies
+    return(list(Proxy.MarkerName=prox, Trait=trait, MVP=mvp_proxies))
+}
+
+# get names of missing snps and their proxies
+miss_snps = unique(subset(xdf_matched_traits, is.na(MVP.alleleA))$MarkerName)
+miss_snps_proxies = subset(snps_page_proxies, Index.MarkerName %in% miss_snps)
+
+# get MVP results for proxy snps
+mvp_proxies = lapply(as.list(miss_snps), 
+                     function(f) .getProxyForSnp(missing_snp=f, 
+                                                 miss_snps_proxies=miss_snps_proxies,
+                                                 xtraits=xtraits,
+                                                 xlist=xlist))
+# name with missing snps
+names(mvp_proxies) = miss_snps
+
+# loop through results 
+for (m in 1:length(mvp_proxies)) {
+    # check whether missing snp had any proxies besides itself
+    if (nrow(mvp_proxies[[m]]$MVP) == 0) {
+        
+        # if yes create a 1 row data frame with NAs
+        mvp_proxies[[m]]$MVP = mvp_proxies[[m]]$MVP[1, ]
+        mvp_proxies[[m]]$MVP$Trait = mvp_proxies[[m]]$Trait
+        mvp_proxies[[m]]$MVP$Index.MarkerName = names(mvp_proxies)[m]
+        
+        # get original results for missing snp
+        x = subset(xdf_matched_traits, MarkerName == names(mvp_proxies)[m])
+        
+        # see which races it was missing from
+        xNA = is.na(x$MVP.alleleA)
+        
+        # add race information
+        if (sum(xNA) > 1) {
+            mvp_proxies[[m]]$MVP = mvp_proxies[[m]]$MVP[1:sum(xNA), ]
+            mvp_proxies[[m]]$MVP[, 1:3] = x[, c(1,5,6)]
+        } else {
+            mvp_proxies[[m]]$MVP$MVP.race = x$MVP.race[xNA]
+        }
+    }
+}
+rm(m, x, xNA)
+
+# collapse to one data frame
+mvp_proxies_df = as.data.frame(do.call(rbind, lapply(mvp_proxies,
+                                                     function(f) f$MVP)))
+rownames(mvp_proxies_df) = NULL
+
+# make unique ids for each missing snp
+flt = subset(xdf_matched_traits, MarkerName %in% miss_snps & is.na(MVP.alleleA))
+flt = apply(flt[, c("Trait", "MarkerName", "MVP.race")], 1,
+            paste0, collapse="_")
+
+# create same ids for proxy results 
+tocheck = apply(mvp_proxies_df[, c("Trait", "Index.MarkerName", "MVP.race")], 1,
+                paste0, collapse="_")
+
+# filter proxy results to only relevatn trait/race combinations
+mvp_proxies_df = mvp_proxies_df[tocheck %in% flt, ]
+
+# plot histograms of proxy MVP p-values for each trait
+par(mfrow=c(2,2))
+xcuts = c(0, 1e-8, 1e-6, 1e-4, 1e-2, 1e-1, 1)
+for (tr in c("HDL","TC","LDL","TG")) {
+    toplot = mvp_proxies_df$Proxy.MVP.frequentist_add_pvalue[mvp_proxies_df$Trait == tr]
+    barplot(table(cut(as.numeric(toplot), xcuts)),
+            main=tr, col='lightgrey')
+    abline(h=5, col='darkgrey')
+}
     
 # ------------------------------------------------------------------------------                                       
 # -- write output, then save workspace with session info
@@ -467,7 +520,10 @@ session_info = sessionInfo()
 save(list=ls(),
      file="lipids_replication_ethnic-specific_noProxiesWORKSPACE.RData")
 
-# # MVP results for proxies of missing PAGE snps
-# write.csv(mvp_proxies_df, row.names=F,
-#           file="PAGE_Lipids_Replication_MVP.ethnic-specific_Proxies_for_missing_updated.csv")
+# MVP results for proxies of missing PAGE snps
+write.csv(mvp_proxies_df, row.names=F,
+          file="PAGE_Lipids_Replication_MVP.ethnic-specific_Proxies_for_missing_updated.csv")
+
+save(list=ls(),
+     file="lipids_replication_ethnic-specific_noProxiesWORKSPACE.RData")
 
