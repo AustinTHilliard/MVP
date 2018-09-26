e2c27a3c42eb5195d962955c638d0459f3357d6e
AustinTHilliard <42285131+AustinTHilliard@users.noreply.github.com>
2018-09-26 15:00:07 -0700

    Updated file paths and comments

--- lipids/lipids_replication_ethnic-specific_updated.R
+++ lipids/lipids_replication_ethnic-specific_updated.R
@@ -6,16 +6,40 @@ rm(list=ls())
 # -- load and prepare PAGE data
 # ------------------------------------------------------------------------------
 # load data and add column to match format of MVP snps
-snps_page = read.csv("PAGE_Lipids_Replication_Table_updated.csv")
+snps_page = read.csv("PAGE_materials/PAGE_Lipids_Replication_Table_updated.csv")
 dim(snps_page)
 # [1] 54  4
+head(snps_page)
+#   Trait        rsID CHR  POS_hg19
+# 1   HDL rs539621506   1 109046381
+# 2   HDL  rs61519011   2 126435003
+# 3   HDL  rs17102282   5 144103408
+# 4   HDL rs373140531   7  89604535
+# 5   HDL  rs11782435   8  13536115
+# 6   HDL rs145312881   9  89053469
 
 snps_page$MarkerName = paste0(snps_page$CHR, ":", snps_page$POS_hg19)
+head(snps_page)
+#   Trait        rsID CHR  POS_hg19  MarkerName
+# 1   HDL rs539621506   1 109046381 1:109046381
+# 2   HDL  rs61519011   2 126435003 2:126435003
+# 3   HDL  rs17102282   5 144103408 5:144103408
+# 4   HDL rs373140531   7  89604535  7:89604535
+# 5   HDL  rs11782435   8  13536115  8:13536115
+# 6   HDL rs145312881   9  89053469  9:89053469
 
 # load proxy snps and add columns to match format of MVP snps
-snps_page_proxies = read.csv("PAGE_Lipids_Replication_Proxies_updated.csv")
+snps_page_proxies = read.csv("PAGE_materials/PAGE_Lipids_Replication_Proxies_updated.csv")
 dim(snps_page_proxies)
 # [1] 716   8
+head(snps_page_proxies)
+#   Trait.s. Chr_indexSNP Pos_indexSNP    IndexSNP Chr_proxy Pos_proxy    ProxySNP R2_index_proxy_snps
+# 1      HDL            1    109046381 rs539621506         1 109046381 rs539621506            1.000000
+# 2      HDL            2    126435003  rs61519011         2 126421304   rs1427309            0.838408
+# 3      HDL            2    126435003  rs61519011         2 126421423   rs5834119            0.838497
+# 4      HDL            2    126435003  rs61519011         2 126422062  rs62158251            0.858461
+# 5      HDL            2    126435003  rs61519011         2 126426024   rs2195073            0.861774
+# 6      HDL            2    126435003  rs61519011         2 126429997   rs1427307            0.838749
 
 # order by trait and index SNP position 
 xord = order(snps_page_proxies$Trait.s., 
@@ -28,6 +52,15 @@ snps_page_proxies$Index.MarkerName = paste0(snps_page_proxies$Chr_indexSNP, ":",
                                             snps_page_proxies$Pos_indexSNP)
 snps_page_proxies$Proxy.MarkerName = paste0(snps_page_proxies$Chr_proxy, ":", 
                                             snps_page_proxies$Pos_proxy)
+head(snps_page_proxies)
+#   Trait.s. Chr_indexSNP Pos_indexSNP    IndexSNP Chr_proxy Pos_proxy    ProxySNP R2_index_proxy_snps Index.MarkerName Proxy.MarkerName
+# 1      HDL            1    109046381 rs539621506         1 109046381 rs539621506            1.000000      1:109046381      1:109046381
+# 2      HDL            2    126435003  rs61519011         2 126421304   rs1427309            0.838408      2:126435003      2:126421304
+# 3      HDL            2    126435003  rs61519011         2 126421423   rs5834119            0.838497      2:126435003      2:126421423
+# 4      HDL            2    126435003  rs61519011         2 126422062  rs62158251            0.858461      2:126435003      2:126422062
+# 5      HDL            2    126435003  rs61519011         2 126426024   rs2195073            0.861774      2:126435003      2:126426024
+# 6      HDL            2    126435003  rs61519011         2 126429997   rs1427307            0.838749      2:126435003      2:126429997
+
 
 # get names of all PAGE snps, including proxies
 c1 = all(snps_page$MarkerName %in% 
@@ -41,13 +74,15 @@ rm(c1, c2)
 
 length(all_snps_page)
 # [1] 700
+head(all_snps_page)
+# [1] "1:109046381" "2:126421304" "2:126421423" "2:126422062" "2:126426024" "2:126429997"
 
 # ------------------------------------------------------------------------------                                       
 # -- MVP - load and filter ethnicity-specific data
 # ------------------------------------------------------------------------------
 # get names of files downloaded from Box:
 #   MVP GWAS/Lipids GWAS/1000G_GWAS_OUT_GENESIS_KLARIN/
-mvp_files = grep("^allchr.*gz$", list.files(), value=T)
+mvp_files = grep("^allchr.*gz$", list.files("MVP_materials/"), value=T)
 mvp_files
 # [1] "allchr..EUR.LDL.results_10_21_17.gz" "allchr.AFR.HDL.results_10_31_17.gz"  "allchr.AFR.LDL.results_10_31_17.gz"  "allchr.AFR.TC.results_10_31_17.gz"   "allchr.AFR.TG.results_10_31_17.gz"  
 # [6] "allchr.EUR.HDL.results_10_31_17.gz"  "allchr.EUR.TC.results_10_31_17.gz"   "allchr.EUR.TG.results_10_31_17.gz"   "allchr.HIS.HDL.results_10_31_17.gz"  "allchr.HIS.LDL.results_10_31_17.gz" 
@@ -64,7 +99,7 @@ for (file in mvp_files) {
     line_num = 0
     
     # read current file one line at a time
-    for (xline in readLines(file)) {
+    for (xline in readLines(paste0("MVP_materials/", file))) {
         # print line number every 1e6 lines
         if (line_num %% 1e6 == 0) 
             cat(line_num, "... ")
@@ -91,10 +126,10 @@ for (file in mvp_files) {
     
     # set dimnames, get colnames from first line (header) of input file
     rownames(xdf) = NULL
-    names(xdf) = strsplit(readLines(file, n=1), " ")[[1]]
+    names(xdf) = strsplit(readLines(paste0("MVP_materials/", file), n=1), " ")[[1]]
     
     # save filtered mvp results to rds file and clean up
-    saveRDS(xdf, file=paste0(file, "_PAGE_updated.rds"))
+    saveRDS(xdf, file=paste0("MVP_materials/", file, "_PAGE_updated.rds"))
     cat("\n")
     rm(line_num, xline, xline_split, snp, xdf)
     gc()
@@ -158,44 +193,58 @@ rm(file)
 # 14: In readLines(file) : line 18418242 appears to contain an embedded nul
 
 # ------------------------------------------------------------------------------                                       
+# The warnings have to do with lines that are incomplete in "allchr.EUR.TC.results_10_31_17.gz"
+# Nothing is affected since none of those lines represent PAGE snps
+# ------------------------------------------------------------------------------                                       
+
+# ------------------------------------------------------------------------------                                       
 # -- MVP - match and merge with primary PAGE snps and group results by trait
 # ------------------------------------------------------------------------------
 # get filenames for filtered mvp files
-mvp_page_files = paste0(mvp_files, "_PAGE.rds")
+mvp_page_files = paste0(mvp_files, "_PAGE_updated.rds")
 cbind(mvp_files, mvp_page_files)
 #       mvp_files                             mvp_page_files                                
-#  [1,] "allchr..EUR.LDL.results_10_21_17.gz" "allchr..EUR.LDL.results_10_21_17.gz_PAGE.rds"
-#  [2,] "allchr.AFR.HDL.results_10_31_17.gz"  "allchr.AFR.HDL.results_10_31_17.gz_PAGE.rds" 
-#  [3,] "allchr.AFR.LDL.results_10_31_17.gz"  "allchr.AFR.LDL.results_10_31_17.gz_PAGE.rds" 
-#  [4,] "allchr.AFR.TC.results_10_31_17.gz"   "allchr.AFR.TC.results_10_31_17.gz_PAGE.rds"  
-#  [5,] "allchr.AFR.TG.results_10_31_17.gz"   "allchr.AFR.TG.results_10_31_17.gz_PAGE.rds"  
-#  [6,] "allchr.EUR.HDL.results_10_31_17.gz"  "allchr.EUR.HDL.results_10_31_17.gz_PAGE.rds" 
-#  [7,] "allchr.EUR.TC.results_10_31_17.gz"   "allchr.EUR.TC.results_10_31_17.gz_PAGE.rds"  
-#  [8,] "allchr.EUR.TG.results_10_31_17.gz"   "allchr.EUR.TG.results_10_31_17.gz_PAGE.rds"  
-#  [9,] "allchr.HIS.HDL.results_10_31_17.gz"  "allchr.HIS.HDL.results_10_31_17.gz_PAGE.rds" 
-# [10,] "allchr.HIS.LDL.results_10_31_17.gz"  "allchr.HIS.LDL.results_10_31_17.gz_PAGE.rds" 
-# [11,] "allchr.HIS.TC.results_10_31_17.gz"   "allchr.HIS.TC.results_10_31_17.gz_PAGE.rds"  
-# [12,] "allchr.HIS.TG.results_10_31_17.gz"   "allchr.HIS.TG.results_10_31_17.gz_PAGE.rds"  
+#  [1,] "allchr..EUR.LDL.results_10_21_17.gz" "allchr..EUR.LDL.results_10_21_17.gz_PAGE_updated.rds"
+#  [2,] "allchr.AFR.HDL.results_10_31_17.gz"  "allchr.AFR.HDL.results_10_31_17.gz_PAGE_updated.rds" 
+#  [3,] "allchr.AFR.LDL.results_10_31_17.gz"  "allchr.AFR.LDL.results_10_31_17.gz_PAGE_updated.rds" 
+#  [4,] "allchr.AFR.TC.results_10_31_17.gz"   "allchr.AFR.TC.results_10_31_17.gz_PAGE_updated.rds"  
+#  [5,] "allchr.AFR.TG.results_10_31_17.gz"   "allchr.AFR.TG.results_10_31_17.gz_PAGE_updated.rds"  
+#  [6,] "allchr.EUR.HDL.results_10_31_17.gz"  "allchr.EUR.HDL.results_10_31_17.gz_PAGE_updated.rds" 
+#  [7,] "allchr.EUR.TC.results_10_31_17.gz"   "allchr.EUR.TC.results_10_31_17.gz_PAGE_updated.rds"  
+#  [8,] "allchr.EUR.TG.results_10_31_17.gz"   "allchr.EUR.TG.results_10_31_17.gz_PAGE_updated.rds"  
+#  [9,] "allchr.HIS.HDL.results_10_31_17.gz"  "allchr.HIS.HDL.results_10_31_17.gz_PAGE_updated.rds" 
+# [10,] "allchr.HIS.LDL.results_10_31_17.gz"  "allchr.HIS.LDL.results_10_31_17.gz_PAGE_updated.rds" 
+# [11,] "allchr.HIS.TC.results_10_31_17.gz"   "allchr.HIS.TC.results_10_31_17.gz_PAGE_updated.rds"  
+# [12,] "allchr.HIS.TG.results_10_31_17.gz"   "allchr.HIS.TG.results_10_31_17.gz_PAGE_updated.rds"  
 
 # reload MVP data matched to PAGE snps (including proxies)
-if (all(mvp_page_files %in% list.files())) {
-    xlist = lapply(as.list(mvp_page_files), readRDS)
+if (all(mvp_page_files %in% list.files("MVP_materials/"))) {
+    xlist = lapply(as.list(paste0("MVP_materials/", mvp_page_files)), readRDS)
     names(xlist) = mvp_page_files
 }
 t(sapply(xlist, dim))
-#                                              [,1] [,2]
-# allchr..EUR.LDL.results_10_21_17.gz_PAGE.rds  554   16
-# allchr.AFR.HDL.results_10_31_17.gz_PAGE.rds   569   16
-# allchr.AFR.LDL.results_10_31_17.gz_PAGE.rds   569   16
-# allchr.AFR.TC.results_10_31_17.gz_PAGE.rds    569   16
-# allchr.AFR.TG.results_10_31_17.gz_PAGE.rds    568   16
-# allchr.EUR.HDL.results_10_31_17.gz_PAGE.rds   554   16
-# allchr.EUR.TC.results_10_31_17.gz_PAGE.rds    545   16
-# allchr.EUR.TG.results_10_31_17.gz_PAGE.rds    554   16
-# allchr.HIS.HDL.results_10_31_17.gz_PAGE.rds   572   16
-# allchr.HIS.LDL.results_10_31_17.gz_PAGE.rds   572   16
-# allchr.HIS.TC.results_10_31_17.gz_PAGE.rds    572   16
-# allchr.HIS.TG.results_10_31_17.gz_PAGE.rds    572   16
+#                                                      [,1] [,2]
+# allchr..EUR.LDL.results_10_21_17.gz_PAGE_updated.rds  656   16
+# allchr.AFR.HDL.results_10_31_17.gz_PAGE_updated.rds   690   16
+# allchr.AFR.LDL.results_10_31_17.gz_PAGE_updated.rds   690   16
+# allchr.AFR.TC.results_10_31_17.gz_PAGE_updated.rds    690   16
+# allchr.AFR.TG.results_10_31_17.gz_PAGE_updated.rds    689   16
+# allchr.EUR.HDL.results_10_31_17.gz_PAGE_updated.rds   656   16
+# allchr.EUR.TC.results_10_31_17.gz_PAGE_updated.rds    647   16
+# allchr.EUR.TG.results_10_31_17.gz_PAGE_updated.rds    655   16
+# allchr.HIS.HDL.results_10_31_17.gz_PAGE_updated.rds   692   16
+# allchr.HIS.LDL.results_10_31_17.gz_PAGE_updated.rds   696   16
+# allchr.HIS.TC.results_10_31_17.gz_PAGE_updated.rds    696   16
+# allchr.HIS.TG.results_10_31_17.gz_PAGE_updated.rds    696   16
+
+head(xlist[[1]])
+#         rsid chromosome position   alleleA alleleB average_maximum_posterior_call     info all_AA  all_AB   all_BB all_total missing_data_proportion cohort_1_hwe frequentist_add_pvalue frequentist_add_beta_1 frequentist_add_se_1
+# 1 1:16072621          1 16072621 GGTTCAATC       G                       0.978903 0.954859 118609 82423.6    14163    215196             1.08195e-15     0.334828               0.349722            -0.00334047            0.0035722
+# 2 1:16074494          1 16074494         C       T                        0.98257 0.962683 118818 82295.7    14082    215196            -5.08515e-14     0.295931               0.304151            -0.00365917           0.00356098
+# 3 1:16075835          1 16075835         C       T                       0.983586 0.964652 118681 82378.7  14136.7    215196            -5.73432e-14     0.323369               0.339895            -0.00339287           0.00355506
+# 4 1:16080501          1 16080501         G       A                        0.99853 0.996087 128183 75850.5  11162.2    215196             8.75701e-14     0.672743                  0.404            -0.00303878           0.00364144
+# 5 1:16080618          1 16080618         G       A                       0.997748 0.993889 128109 75897.4  11189.3    215196              5.6667e-14     0.709041               0.418954            -0.00294518           0.00364395
+# 6 2:17471785          2 17471785         G       A                       0.999779 0.923304 214668 527.752 0.015996    215196            -6.53902e-14            1               0.766482                0.01347            0.0453567
 
 # fix names with two dots
 names(xlist) = gsub("..", ".", names(xlist), fixed=T)
@@ -223,6 +272,15 @@ t(sapply(xlist_matched, dim))
 # allchr.HIS.LDL.results_10_31_17.gz_PAGE.rds   54   18
 # allchr.HIS.TC.results_10_31_17.gz_PAGE.rds    54   18
 # allchr.HIS.TG.results_10_31_17.gz_PAGE.rds    54   18
+
+head(xlist_matched[[1]])
+#   Trait        rsID CHR  POS_hg19  MarkerName alleleA alleleB average_maximum_posterior_call     info  all_AA  all_AB  all_BB all_total missing_data_proportion cohort_1_hwe frequentist_add_pvalue frequentist_add_beta_1 frequentist_add_se_1
+# 1   HDL rs539621506   1 109046381 1:109046381    <NA>    <NA>                           <NA>     <NA>    <NA>    <NA>    <NA>      <NA>                    <NA>         <NA>                   <NA>                   <NA>                 <NA>
+# 2   HDL  rs61519011   2 126435003 2:126435003       G      GT                       0.949273  0.90945 24256.3 95812.6 95127.1    215196             -4.0573e-15     0.578126               0.422256             0.00271522           0.00338338
+# 3   HDL  rs17102282   5 144103408 5:144103408       G       A                       0.997997 0.991909  162430 49051.5 3714.24    215196            -9.00045e-14     0.872317               0.112884             0.00718486           0.00453198
+# 4   HDL rs373140531   7  89604535  7:89604535    <NA>    <NA>                           <NA>     <NA>    <NA>    <NA>    <NA>      <NA>                    <NA>         <NA>                   <NA>                   <NA>                 <NA>
+# 5   HDL  rs11782435   8  13536115  8:13536115       C       T                       0.954946 0.897483  131388 73596.2 10211.5    215196             1.17662e-14     0.467829               0.689281            -0.00155833           0.00389746
+# 6   HDL rs145312881   9  89053469  9:89053469    <NA>    <NA>                           <NA>     <NA>    <NA>    <NA>    <NA>      <NA>                    <NA>         <NA>                   <NA>                   <NA>                 <NA>
 
 # define the trait represented by each dataset
 # -- why not just strsplit to get traits from names(xlist)? --
@@ -273,6 +331,15 @@ if (all(sapply(xlist_matched_traits, nrow) == table(snps_page$Trait) * 3)) {
     print("ok")
 }
 
+head(xlist_matched_traits[[1]])
+#   Trait        rsID CHR  POS_hg19   MarkerName MVP.race MVP.alleleA MVP.alleleB MVP.average_maximum_posterior_call MVP.info MVP.all_AA MVP.all_AB  MVP.all_BB MVP.all_total MVP.missing_data_proportion MVP.cohort_1_hwe MVP.frequentist_add_pvalue MVP.frequentist_add_beta_1 MVP.frequentist_add_se_1
+# 1   HDL  rs10430621  10 133667886 10:133667886      AFR           T           C                           0.908819 0.803486    33644.7    20121.3     3066.94         56833                -5.56902e-15         0.422009                   0.383299                 0.00683041               0.00783448
+# 2   HDL  rs10430621  10 133667886 10:133667886      EUR        <NA>        <NA>                               <NA>     <NA>       <NA>       <NA>        <NA>          <NA>                        <NA>             <NA>                       <NA>                       <NA>                     <NA>
+# 3   HDL  rs10430621  10 133667886 10:133667886      HIS        <NA>        <NA>                               <NA>     <NA>       <NA>       <NA>        <NA>          <NA>                        <NA>             <NA>                       <NA>                       <NA>                     <NA>
+# 4   HDL rs115207757  17  79371257  17:79371257      AFR           G           A                            0.99794 0.843562    56250.3    579.697     2.98248         56833                -3.42463e-14          0.66361                   0.937074                 0.00355046                0.0449722
+# 5   HDL rs115207757  17  79371257  17:79371257      EUR        <NA>        <NA>                               <NA>     <NA>       <NA>       <NA>        <NA>          <NA>                        <NA>             <NA>                       <NA>                       <NA>                     <NA>
+# 6   HDL rs115207757  17  79371257  17:79371257      HIS           G           A                           0.999749 0.903271    23893.5    52.5035 0.000999001         23946                  2.8106e-15                1                   0.453438                   0.108987                 0.145373
+
 # collapse resulting list to a single data frame
 xdf_matched_traits = as.data.frame(do.call(rbind, xlist_matched_traits))
 rownames(xdf_matched_traits) = NULL
@@ -281,23 +348,23 @@ dim(xdf_matched_traits)
 # [1] 162  19
 
 # order based on trait, chromosome, and race
-xord = order(xdf_matched_traits$Trait, xdf_matched_traits$CHR, xdf_matched_traits$MVP.race)
+xord = order(xdf_matched_traits$Trait, xdf_matched_traits$MarkerName, xdf_matched_traits$MVP.race)
 xdf_matched_traits = xdf_matched_traits[xord, ]
 rm(xord)
 
 # look at distributions of race and traits
 table(subset(xdf_matched_traits, is.na(MVP.alleleA))$MVP.race)
 # AFR EUR HIS 
-#  19  27  19 
+#   2  17   3 
 
 table(subset(xdf_matched_traits, is.na(MVP.alleleA))$Trait)
 # HDL LDL  TC  TG 
-#  21   5  14  25 
+#  14   2   3   3 
 
 sort(table(apply(xdf_matched_traits[,c("Trait","MVP.race")], 
                  1, paste0, collapse=":")[is.na(xdf_matched_traits$MVP.alleleA)]))
-# LDL:AFR LDL:HIS LDL:EUR  TC:AFR  TC:HIS HDL:AFR HDL:HIS  TC:EUR  TG:AFR  TG:HIS HDL:EUR  TG:EUR 
-#       1       1       3       4       4       6       6       6       8       8       9       9 
+# HDL:AFR LDL:EUR HDL:HIS  TC:EUR  TG:EUR HDL:EUR 
+#       2       2       3       3       3       9
 
 # plot histograms of MVP p-values for each trait
 par(mfrow=c(2,2))
@@ -428,7 +495,7 @@ flt = apply(flt[, c("Trait", "MarkerName", "MVP.race")], 1,
 tocheck = apply(mvp_proxies_df[, c("Trait", "Index.MarkerName", "MVP.race")], 1,
                 paste0, collapse="_")
 
-# filter proxy results to only relevant trait/race combinations
+# filter proxy results to only relevatn trait/race combinations
 mvp_proxies_df = mvp_proxies_df[tocheck %in% flt, ]
 
 # plot histograms of proxy MVP p-values for each trait
@@ -446,19 +513,17 @@ for (tr in c("HDL","TC","LDL","TG")) {
 # ------------------------------------------------------------------------------
 # MVP results for primary PAGE snps
 write.csv(xdf_matched_traits, row.names=F,
-          file="PAGE_Lipids_Replication_MVP.ethnic-specific_noProxies_updated.csv")
+          file="PAGE_Lipids_Replication_MVP.ethnic-specific_noProxies_updated_new.csv")
+
+# MVP results for proxies of missing PAGE snps
+write.csv(mvp_proxies_df, row.names=F,
+          file="PAGE_Lipids_Replication_MVP.ethnic-specific_Proxies_for_missing_updated_new.csv")
+
 
 # get session info
 session_info = sessionInfo()
 
 # workspace 
 save(list=ls(),
-     file="lipids_replication_ethnic-specific_noProxiesWORKSPACE.RData")
-
-# MVP results for proxies of missing PAGE snps
-write.csv(mvp_proxies_df, row.names=F,
-          file="PAGE_Lipids_Replication_MVP.ethnic-specific_Proxies_for_missing_updated.csv")
-
-save(list=ls(),
-     file="lipids_replication_ethnic-specific_noProxiesWORKSPACE.RData")
+     file="lipids_replication_ethnic-specific_WORKSPACE_updated_new.RData")
 
